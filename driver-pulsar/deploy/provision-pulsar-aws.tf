0be12e086e6e73cf5ba9b0a507317761371b90c1
Luc Perkins <lucperkins@gmail.com>
2017-12-15 13:58:54 -0800

    add default ansible configs

--- driver-pulsar/deploy/provision-pulsar-aws.tf
+++ driver-pulsar/deploy/provision-pulsar-aws.tf
@@ -1,10 +1,11 @@
 variable "public_key_path" {
+  default = "~/.ssh/id_rsa.pub"
   description = <<DESCRIPTION
 Path to the SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
 
-Example: ~/.ssh/terraform.pub
+Example: ~/.ssh/id_rsa.pub
 DESCRIPTION
 }
 
