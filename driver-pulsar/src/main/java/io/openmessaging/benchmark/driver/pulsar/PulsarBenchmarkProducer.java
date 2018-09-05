a9a53254bae5a2b24251b284784a20408e58cc48
Franck NEGRELLO <franck.negrello@gmail.com>
2018-09-05 20:48:21 +0200

    Upgrade pulsar version to 2.1.0-incubating #75 (#76)
    Upgrading pulsar driver with the latest version of pulsar client 2.1.0-incubating.
    This version is backward compatible with pulsar 1.22.0

--- driver-pulsar/src/main/java/io/openmessaging/benchmark/driver/pulsar/PulsarBenchmarkProducer.java
+++ driver-pulsar/src/main/java/io/openmessaging/benchmark/driver/pulsar/PulsarBenchmarkProducer.java
@@ -21,16 +21,16 @@ package io.openmessaging.benchmark.driver.pulsar;
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
 
-import org.apache.pulsar.client.api.MessageBuilder;
 import org.apache.pulsar.client.api.Producer;
+import org.apache.pulsar.client.api.TypedMessageBuilder;
 
 import io.openmessaging.benchmark.driver.BenchmarkProducer;
 
 public class PulsarBenchmarkProducer implements BenchmarkProducer {
 
-    private final Producer producer;
+    private final Producer<byte[]> producer;
 
-    public PulsarBenchmarkProducer(Producer producer) {
+    public PulsarBenchmarkProducer(Producer<byte[]> producer) {
         this.producer = producer;
     }
 
@@ -41,12 +41,12 @@ public class PulsarBenchmarkProducer implements BenchmarkProducer {
 
     @Override
     public CompletableFuture<Void> sendAsync(Optional<String> key, byte[] payload) {
-        MessageBuilder msgBuilder = MessageBuilder.create().setContent(payload);
+        TypedMessageBuilder<byte[]> msgBuilder = producer.newMessage().value(payload);
         if (key.isPresent()) {
-            msgBuilder.setKey(key.get());
+            msgBuilder.key(key.get());
         }
 
-        return producer.sendAsync(msgBuilder.build()).thenApply(msgId -> null);
+        return msgBuilder.sendAsync().thenApply(msgId -> null);
     }
 
 }
