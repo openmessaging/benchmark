7605ada95de526711cf945899b4253f40bd7b5cd
rudy steiner <rudy_steiner@163.com>
2019-05-24 11:58:11 +0800

    kafka aware auto commit offset config

--- driver-kafka/src/main/java/io/openmessaging/benchmark/driver/kafka/KafkaBenchmarkConsumer.java
+++ driver-kafka/src/main/java/io/openmessaging/benchmark/driver/kafka/KafkaBenchmarkConsumer.java
@@ -20,10 +20,12 @@ package io.openmessaging.benchmark.driver.kafka;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
@@ -44,11 +46,11 @@ public class KafkaBenchmarkConsumer implements BenchmarkConsumer {
     private final ExecutorService executor;
     private final Future<?> consumerTask;
     private volatile boolean closing = false;
-
-    public KafkaBenchmarkConsumer(KafkaConsumer<String, byte[]> consumer, ConsumerCallback callback) {
+    private boolean autoCommit;
+    public KafkaBenchmarkConsumer(KafkaConsumer<String, byte[]> consumer, Properties consumerConfig, ConsumerCallback callback) {
         this.consumer = consumer;
         this.executor = Executors.newSingleThreadExecutor();
-
+        this.autoCommit= Boolean.valueOf((String)consumerConfig.getOrDefault(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG,"false"));
         this.consumerTask = this.executor.submit(() -> {
             while (!closing) {
                 try {
@@ -59,10 +61,10 @@ public class KafkaBenchmarkConsumer implements BenchmarkConsumer {
                         callback.messageReceived(record.value(), record.timestamp());
 
                         offsetMap.put(new TopicPartition(record.topic(), record.partition()),
-                            new OffsetAndMetadata(record.offset()));
+                            new OffsetAndMetadata(record.offset()+1));
                     }
 
-                    if (!offsetMap.isEmpty()) {
+                    if (!autoCommit&&!offsetMap.isEmpty()) {
                         consumer.commitSync(offsetMap);
                     }
                 }catch(Exception e){
