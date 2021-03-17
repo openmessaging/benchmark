461cf89c1493fb18da9a8d25c7a5c28ec391cf97
Raúl Gracia <raul.gracia@emc.com>
2021-03-17 20:22:59 +0100

    Initial commit of Pravega driver.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

---
+++ driver-pravega/deploy/open_saved_metrics/open_saved_metrics.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+
+set -e
+
+: ${1?"Usage: $0 PRAVEGA_LOGS_DIRECTORY"}
+
+pravega_logs_dir=$(readlink -f "$1")
+echo Loading metrics from: ${pravega_logs_dir}
+export INFLUXDB_BACKUP_DIR="${pravega_logs_dir}/influxdb"
+export PROMETHEUS_DATA_DIR="${pravega_logs_dir}/prometheus"
+docker-compose -p open_saved_metrics -f $(dirname $0)/docker-compose.yml rm --stop --force influxdb
+
+#
+# InfluxDB
+#
+
+influxdb_tgz="${pravega_logs_dir}/influxdb.tgz"
+ls -lh ${influxdb_tgz}
+rm -rf "${INFLUXDB_BACKUP_DIR}"
+mkdir "${INFLUXDB_BACKUP_DIR}"
+tar -xzvf "${influxdb_tgz}" -C "${INFLUXDB_BACKUP_DIR}"
+docker-compose -p open_saved_metrics -f $(dirname $0)/docker-compose.yml up -d influxdb
+docker-compose -p open_saved_metrics -f $(dirname $0)/docker-compose.yml exec influxdb influxd restore -portable /tmp/backup
+
+#
+# Prometheus
+#
+
+prometheus_tgz="${pravega_logs_dir}/prometheus.tgz"
+ls -lh ${prometheus_tgz}
+rm -rf "${PROMETHEUS_DATA_DIR}"
+mkdir "${PROMETHEUS_DATA_DIR}"
+tar -xzvf "${prometheus_tgz}" --strip-components=3 -C "${PROMETHEUS_DATA_DIR}" opt/prometheus/data
+chmod -R a+rwX "${PROMETHEUS_DATA_DIR}"
+docker-compose -p open_saved_metrics -f $(dirname $0)/docker-compose.yml up -d prometheus
+
+#
+# Grafana
+#
+
+docker-compose -p open_saved_metrics -f $(dirname $0)/docker-compose.yml up -d
+
+echo
+echo Grafana URL: http://localhost:3000
+echo
---
+++ driver-pravega/doc/build_pravega.md
@@ -0,0 +1,30 @@
+## Build Pravega
+
+To use a pre-release version of Pravega, you must build it manually using the following steps:
+
+```
+git clone https://github.com/pravega/pravega
+cd pravega
+git checkout master
+./gradlew install distTar
+```
+
+This will build the file `pravega/build/distributions/pravega-0.9.0.tgz.`
+Then comment `pravegaSrc` and `pravegaSrcRemote: yes` and uncomment `pravegaSrc` `pravegaSrcRemote: no` in `driver-pravega/deploy/deploy.yaml`
+```
+# Change below to use a published release of Pravega or a local build.
+# pravegaSrc: "https://github.com/pravega/pravega/releases/download/v{{ pravegaVersion }}/pravega-{{ pravegaVersion }}.tgz"
+# pravegaSrcRemote: yes
+# Here is the file path for local Pravega build
+pravegaSrc: "../../../pravega/build/distributions/pravega-{{ pravegaVersion }}.tgz"
+pravegaSrcRemote: no
+```
+If needed, change the variable `pravegaVersion` in [vars.yaml](../deploy/vars.yaml) to match the version built.
+
+If needed, change [pom.xml](../pom.xml) to match the version built.
+
+## Build Benchmark
+Add flag to skip license check`-Dlicense.skip=true` if license check failed.
+```
+mvn clean install
+```
