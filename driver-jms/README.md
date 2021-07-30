224d78e621ed5dd531dce2f5a72bd07c3038d139
Enrico Olivelli <eolivelli@gmail.com>
2021-07-30 19:53:01 +0200

    Add JMS Driver (#194)
    * Add JMS Driver
    
    * fix readme
    
    * Update driver-jms/kafka-jms.yaml
    
    Co-authored-by: Yuto Furuta <mzq6mft9zz@gmail.com>
    
    * add newline
    
    Co-authored-by: Enrico Olivelli <eolivelli@apache.org>
    Co-authored-by: Yuto Furuta <mzq6mft9zz@gmail.com>

---
+++ driver-jms/README.md
@@ -0,0 +1,5 @@
+# General Purpose JMS benchmark
+
+With this driver you can run the benchmark against any messaging system that supports JMS 1.1 or JMS 2.0 API.
+
+For instructions on running the OpenMessaging benchmarks for JMS, see the [official documentation](http://openmessaging.cloud/docs/benchmarks/jms).
---
+++ driver-jms/src/main/java/io/openmessaging/benchmark/driver/jms/JMSBenchmarkProducer.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.openmessaging.benchmark.driver.jms;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+import javax.jms.BytesMessage;
+import javax.jms.CompletionListener;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.JMSProducer;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import io.openmessaging.benchmark.driver.jms.config.JMSConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.openmessaging.benchmark.driver.BenchmarkProducer;
+
+public class JMSBenchmarkProducer implements BenchmarkProducer {
+
+    private final Session session;
+    private final Destination destination;
+    private final MessageProducer producer;
+    private final boolean useAsyncSend;
+    private final List<JMSConfig.AddProperty> properties;
+    public JMSBenchmarkProducer(Session session, Destination destination, boolean useAsyncSend, List<JMSConfig.AddProperty> properties) throws Exception {
+        this.session = session;
+        this.destination = destination;
+        this.useAsyncSend = useAsyncSend;
+        this.producer = session.createProducer(destination);
+        this.properties = properties != null ? properties : Collections.emptyList();
+    }
+
+    @Override
+    public void close() throws Exception {
+        session.close();
+    }
+
+    @Override
+    public CompletableFuture<Void> sendAsync(Optional<String> key, byte[] payload) {
+        CompletableFuture<Void> res = new CompletableFuture<>();
+        try
+        {
+            BytesMessage bytesMessage = session.createBytesMessage();
+            bytesMessage.writeBytes(payload);
+            if (key.isPresent())
+            {
+                // a behaviour similar to https://activemq.apache.org/message-groups
+                bytesMessage.setStringProperty("JMSXGroupID", key.get());
+            }
+            for (JMSConfig.AddProperty prop : properties) {
+                bytesMessage.setStringProperty(prop.name, prop.value);
+            }
+            if (useAsyncSend) {
+                producer.send(bytesMessage, new CompletionListener()
+                {
+                    @Override
+                    public void onCompletion(Message message)
+                    {
+                        res.complete(null);
+                    }
+
+                    @Override
+                    public void onException(Message message, Exception exception)
+                    {
+                        log.info("send completed with error", exception);
+                        res.completeExceptionally(exception);
+                    }
+                });
+            } else {
+                producer.send(bytesMessage);
+                res.complete(null);
+            }
+        } catch (JMSException err) {
+            res.completeExceptionally(err);
+        }
+        return res;
+    }
+    private static final Logger log = LoggerFactory.getLogger(JMSBenchmarkProducer.class);
+}
