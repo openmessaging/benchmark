854f73d1fea5c19366ac23dc5d831e29890c5389
filipe oliveira <filipecosta.90@gmail.com>
2021-09-24 05:33:10 +0100

    Initial Redis support (#196)

---
+++ driver-redis/src/main/java/io/openmessaging/benchmark/driver/redis/RedisBenchmarkProducer.java
@@ -0,0 +1,64 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.openmessaging.benchmark.driver.redis;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import io.openmessaging.benchmark.driver.BenchmarkProducer;
+import redis.clients.jedis.*;
+import redis.clients.jedis.params.XAddParams;
+
+public class RedisBenchmarkProducer implements BenchmarkProducer {
+    private final JedisPool pool;
+    private final String rmqTopic;
+    private final XAddParams xaddParams;
+
+    public RedisBenchmarkProducer(final JedisPool pool, final String rmqTopic) {
+        this.pool = pool;
+        this.rmqTopic = rmqTopic;
+        this.xaddParams = redis.clients.jedis.params.XAddParams.xAddParams();
+    }
+
+    @Override
+    public CompletableFuture<Void> sendAsync(final Optional<String> key, final byte[] payload) {
+        Map<byte[], byte[]> map1 = new HashMap<>();
+        map1.put("payload".getBytes(), payload);
+
+        if (key.isPresent()) {
+            map1.put("key".getBytes(), key.toString().getBytes());
+        }
+
+        CompletableFuture<Void> future = new CompletableFuture<>();
+            try (Jedis jedis = this.pool.getResource()) {
+                jedis.xadd(this.rmqTopic.getBytes(),map1, this.xaddParams);
+                future.complete(null);
+            } catch (Exception e) {
+                future.completeExceptionally(e);
+            }
+        return future;
+    }
+
+    @Override
+    public void close() throws Exception {
+        // Close in Driver
+    }
+}
