baa7bcdc87c5576cd4365284b01be0beab7b2668
lipenghui <penghui@apache.org>
2021-11-16 10:16:21 +0800

    Allow skip Journal for Apache Pulsar (#210)
    * Allow skip Journal for Apache Pulsar
    
    - Allow skip Journal by using -e skip_journal=true
    - Fix Pulsar dashboard
    
    * Fix typo.
    
    * Set write and read thread to 8.
    
    * Revert some params changes.

--- driver-pulsar/deploy/ssd/templates/pulsar_env.sh
+++ driver-pulsar/deploy/ssd/templates/pulsar_env.sh
@@ -45,7 +45,7 @@
 PULSAR_MEM=" -Xms{{ max_heap_memory }} -Xmx{{ max_heap_memory }} -XX:MaxDirectMemorySize={{ max_direct_memory }}"
 
 # Garbage collection options
-PULSAR_GC=" -XX:+UseG1GC -XX:MaxGCPauseMillis=10 -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+AggressiveOpts -XX:+DoEscapeAnalysis -XX:ParallelGCThreads=32 -XX:ConcGCThreads=32 -XX:G1NewSizePercent=50 -XX:+DisableExplicitGC -XX:-ResizePLAB"
+PULSAR_GC=" -XX:+UnlockExperimentalVMOptions -XX:+UseZGC -XX:+ParallelRefProcEnabled -XX:+AggressiveOpts -XX:+DoEscapeAnalysis -XX:ParallelGCThreads=12 -XX:ConcGCThreads=12 -XX:+DisableExplicitGC -XX:-ResizePLAB"
 PULSAR_GC="${PULSAR_GC} -XX:+PerfDisableSharedMem -XX:+AlwaysPreTouch -XX:-UseBiasedLocking"
 
 # Extra options to be passed to the jvm
