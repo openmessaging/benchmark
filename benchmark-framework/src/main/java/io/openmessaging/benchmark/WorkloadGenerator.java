a8c36cb341be337e2609e97902ff857db62e493f
Dave Fisher <dave.fisher@datastax.com>
2022-01-19 12:32:27 -0800

    Check for Backlog drain complete every 1 sec

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/WorkloadGenerator.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/WorkloadGenerator.java
@@ -386,7 +386,7 @@ public class WorkloadGenerator implements AutoCloseable {
             }
 
             try {
-                Thread.sleep(100);
+                Thread.sleep(1000);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
