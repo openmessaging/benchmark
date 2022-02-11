c93e89b28f81250a85a84b3f69c86312e4eb2c8b
Dave Fisher <dave.fisher@datastax.com>
2022-02-11 12:46:02 -0800

    Updated versions and new workloads

--- driver-pulsar/deploy/provision-pulsar-aws.tf
+++ driver-pulsar/deploy/provision-pulsar-aws.tf
@@ -171,9 +171,9 @@ resource "aws_instance" "prometheus" {
 }
 
 output "client_ssh_host" {
-  value = "${aws_instance.client.0.public_ip}"
+  value = aws_instance.client.0.public_ip
 }
 
 output "prometheus_host" {
-  value = "${aws_instance.prometheus.0.public_ip}"
+  value = aws_instance.prometheus.0.public_ip
 }
