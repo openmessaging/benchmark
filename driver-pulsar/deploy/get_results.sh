858549274a95b789a7ee229eb2df7918121db21f
Dave Fisher <dave.fisher@datastax.com>
2022-02-16 09:59:50 -0800

    Update broker configuration for Jetty max number of threads

---
+++ driver-pulsar/deploy/get_results.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export here=`pwd`
+export TF_STATE=.
+cd ${1}
+echo "Get Results ${1} ${2}"
+export ssh_host=`terraform output client_ssh_host`
+export ssh_host=`sed -e 's/^"//' -e 's/"$//' <<<"$ssh_host"`
+cd ${here}
+export tdate=`date -u "+%Y-%m-%d"`
+export results=${2}/${tdate}
+mkdir -p ${results}
+scp -i ~/.ssh/pulsar_aws ec2-user@${ssh_host}:/opt/benchmark/\*.json ${results}
+
