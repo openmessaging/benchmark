f051df762643a788ce13141d18c50e7bbf260e6b
dingbo <dingbo8128@163.com>
2022-07-05 21:00:31 +0800

    tdengine driver develop

--- driver-tdengine/src/main/java/io/openmessaging/benchmark/driver/tdengine/TDengineBenchmarkDriver.java
+++ driver-tdengine/src/main/java/io/openmessaging/benchmark/driver/tdengine/TDengineBenchmarkDriver.java
@@ -5,6 +5,8 @@ import io.openmessaging.benchmark.driver.BenchmarkDriver;
 import io.openmessaging.benchmark.driver.BenchmarkProducer;
 import io.openmessaging.benchmark.driver.ConsumerCallback;
 import org.apache.bookkeeper.stats.StatsLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +15,7 @@ import java.util.concurrent.CompletableFuture;
 public class TDengineBenchmarkDriver implements BenchmarkDriver {
     @Override
     public void initialize(File configurationFile, StatsLogger statsLogger) throws IOException {
-
+        log.debug("initialize");
     }
 
     @Override
@@ -23,21 +25,28 @@ public class TDengineBenchmarkDriver implements BenchmarkDriver {
 
     @Override
     public CompletableFuture<Void> createTopic(String topic, int partitions) {
-        return null;
+        return CompletableFuture.runAsync(() -> {
+            log.debug("create topic {}", topic);
+        });
     }
 
     @Override
     public CompletableFuture<BenchmarkProducer> createProducer(String topic) {
-        return null;
+        log.debug("createProducer topic {}", topic);
+        return CompletableFuture.completedFuture(new TDengineBenchmarkProducer());
     }
 
     @Override
     public CompletableFuture<BenchmarkConsumer> createConsumer(String topic, String subscriptionName, ConsumerCallback consumerCallback) {
-        return null;
+        log.debug("createConsumer topic {} subscriptionName {}", topic, subscriptionName);
+        return CompletableFuture.completedFuture(new TDengineBenchmarkConsumer());
     }
 
     @Override
     public void close() throws Exception {
+        log.debug("close");
 
     }
+
+    private static final Logger log = LoggerFactory.getLogger(TDengineBenchmarkDriver.class);
 }
--- driver-tdengine/src/test/java/io/openmessaging/benchmark/driver/tdengine/BenchmarkLocalRunner.java
+++ driver-tdengine/src/test/java/io/openmessaging/benchmark/driver/tdengine/BenchmarkLocalRunner.java
@@ -3,6 +3,6 @@ import io.openmessaging.benchmark.Benchmark;
 
 public class BenchmarkLocalRunner {
     public static void main(String[] args) throws Exception {
-        Benchmark.main(new String[] {});
+        Benchmark.main(new String[] {"--drivers", "driver-tdengine/tdengine.yaml","workloads/1-topic-16-partitions-1kb.yaml"});
     }
 }
