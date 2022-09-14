000f9c4dbcb75e4e556acd3ff0b2f551963b5523
teabot <elliot.west@streamnative.io>
2022-09-14 14:29:56 +0100

    Introduce AZ var, default to us-west-2a

--- driver-rabbitmq/deploy/provision-rabbitmq-aws.tf
+++ driver-rabbitmq/deploy/provision-rabbitmq-aws.tf
@@ -22,6 +22,7 @@ variable "num_instances" {
 }
 
 variable "region" {}
+variable "az" {}
 variable "ami" {}
 
 provider "aws" {
@@ -54,6 +55,7 @@ resource "aws_subnet" "benchmark_subnet" {
   vpc_id                  = "${aws_vpc.benchmark_vpc.id}"
   cidr_block              = "10.0.0.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = var.az
 }
 
 resource "aws_security_group" "benchmark_security_group" {
