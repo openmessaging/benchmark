82cb5a8386f7811949bc226fe68e16f637b6a65f
teabot <elliot.west@streamnative.io>
2022-10-14 10:30:51 +0100

    → AWS, +Prometheus +Grafana, +doc, versions++
    Install NATS metrics exporters
    
    NATS exporter, format data disks
    
    disks, monitoring
    
    Mount NATS dashboards
    
    streaming vs JetStream
    
    Typo
    
    Start exporter after NATS
    
    Correct docker usage
    
    Correct working directory
    
    Unambiguous name
    
    Include NATs, reference STAN
    
    Fix scrape port
    
    Formatting
    
    License

---
+++ driver-nats-streaming/deploy/provision-nats-streaming-aws.tf
@@ -0,0 +1,161 @@
+variable "public_key_path" {
+  description = <<DESCRIPTION
+Path to the SSH public key to be used for authentication.
+Ensure this keypair is added to your local SSH agent so provisioners can
+connect.
+
+Example: ~/.ssh/nats_streaming_aws.pub
+DESCRIPTION
+}
+
+variable "key_name" {
+  default     = "nats-streaming-benchmark-key"
+  description = "Desired name of AWS key pair"
+}
+
+variable "instance_types" {
+  type = map(string)
+}
+
+variable "num_instances" {
+  type = map(string)
+}
+
+variable "region" {}
+variable "az" {}
+variable "ami" {}
+
+provider "aws" {
+  region = var.region
+}
+
+# Create a VPC to launch our instances into
+resource "aws_vpc" "benchmark_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "Benchmark-VPC"
+  }
+}
+
+# Create an internet gateway to give our subnet access to the outside world
+resource "aws_internet_gateway" "default" {
+  vpc_id = aws_vpc.benchmark_vpc.id
+}
+
+# Grant the VPC internet access on its main route table
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.benchmark_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.default.id
+}
+
+# Create a subnet to launch our instances into
+resource "aws_subnet" "benchmark_subnet" {
+  vpc_id                  = aws_vpc.benchmark_vpc.id
+  cidr_block              = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = var.az
+}
+
+resource "aws_security_group" "benchmark_security_group" {
+  name   = "terraform"
+  vpc_id = aws_vpc.benchmark_vpc.id
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus access
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Grafana access
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # All ports open within the VPC
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Benchmark-Security-Group-NATS-Streaming"
+  }
+}
+
+resource "aws_key_pair" "auth" {
+  key_name   = var.key_name
+  public_key = file(var.public_key_path)
+}
+
+resource "aws_instance" "nats" {
+  ami                    = var.ami
+  instance_type          = var.instance_types["nats"]
+  key_name               = aws_key_pair.auth.id
+  subnet_id              = aws_subnet.benchmark_subnet.id
+  vpc_security_group_ids = [aws_security_group.benchmark_security_group.id]
+  count                  = var.num_instances["nats"]
+
+  tags = {
+    Name = "nats_streaming_${count.index}"
+  }
+}
+
+resource "aws_instance" "client" {
+  ami                    = var.ami
+  instance_type          = var.instance_types["client"]
+  key_name               = aws_key_pair.auth.id
+  subnet_id              = aws_subnet.benchmark_subnet.id
+  vpc_security_group_ids = [aws_security_group.benchmark_security_group.id]
+  count                  = var.num_instances["client"]
+
+  tags = {
+    Name = "nats_streaming_client_${count.index}"
+  }
+}
+
+resource "aws_instance" "prometheus" {
+  ami                    = var.ami
+  instance_type          = var.instance_types["prometheus"]
+  key_name               = aws_key_pair.auth.id
+  subnet_id              = aws_subnet.benchmark_subnet.id
+  vpc_security_group_ids = [
+    aws_security_group.benchmark_security_group.id]
+  count = var.num_instances["prometheus"]
+
+  tags = {
+    Name = "prometheus_${count.index}"
+  }
+}
+
+output "prometheus_host" {
+  value = aws_instance.prometheus.0.public_ip
+}
+
+output "client_ssh_host" {
+  value = aws_instance.client[0].public_ip
+}
