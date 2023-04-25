dacee7e993a1aed4d4074d789fb0e19448b9ce5c
Lishen Yao <yaalsn@gmail.com>
2023-04-25 16:37:53 +0800

    Change hint max rate times

--- benchmark-framework/src/main/java/io/openmessaging/benchmark/RateController.java
+++ benchmark-framework/src/main/java/io/openmessaging/benchmark/RateController.java
@@ -83,7 +83,7 @@ class RateController {
             rampDown();
             hintMaxRateTimes += 1;
 
-            if (hintMaxRateTimes > 1) {
+            if (hintMaxRateTimes > 10) {
                 maxRate = rate;
                 log.info("Exceed max rate for 2 times, decrease rate from {} to {}", rate, rate * 0.8);
                 hintMaxRateTimes = 0;
